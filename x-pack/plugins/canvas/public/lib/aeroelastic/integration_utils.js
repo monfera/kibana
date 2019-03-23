ee0865908ca745c25ead0286df28c8b49e6ef194
monfera <monfera.robert@gmail.com>
2019-03-23 21:07:53 +0100

    moved out some reusable things into integration_utils

--- x-pack/plugins/canvas/public/lib/aeroelastic/integration_utils.js
+++ x-pack/plugins/canvas/public/lib/aeroelastic/integration_utils.js
@@ -5,14 +5,48 @@
  */
 
 import { shallowEqual } from 'recompose';
-import { aeroelastic as aero, aeroelasticConfiguration } from '../aeroelastic_kibana';
+import { aeroelastic as aero } from '../aeroelastic_kibana';
 import { getNodes, getSelectedElement, getSelectedPage } from '../../state/selectors/workpad';
 import { addElement, removeElements, setMultiplePositions } from '../../state/actions/elements';
 import { selectElement } from '../../state/actions/transient';
 import { matrixToAngle } from './matrix';
 import { arrayToMap, flatten, identity } from './functional';
 import { getLocalTransformMatrix } from './layout_functions';
+import { createLayoutStore } from './index';
 
+export const aeroelasticConfiguration = {
+  getAdHocChildAnnotationName: 'adHocChildAnnotation',
+  adHocGroupName: 'adHocGroup',
+  alignmentGuideName: 'alignmentGuide',
+  atopZ: 1000,
+  depthSelect: true,
+  devColor: 'magenta',
+  groupName: 'group',
+  groupResize: true,
+  guideDistance: 3,
+  hoverAnnotationName: 'hoverAnnotation',
+  hoverLift: 100,
+  intraGroupManipulation: false,
+  intraGroupSnapOnly: false,
+  minimumElementSize: 2,
+  persistentGroupName: 'persistentGroup',
+  resizeAnnotationConnectorOffset: 0,
+  resizeAnnotationOffset: 0,
+  resizeAnnotationOffsetZ: 0.1, // causes resize markers to be slightly above the shape plane
+  resizeAnnotationSize: 10,
+  resizeConnectorName: 'resizeConnector',
+  resizeHandleName: 'resizeHandle',
+  rotateAnnotationOffset: 12,
+  rotateSnapInPixels: 10,
+  rotationEpsilon: 0.001,
+  rotationHandleName: 'rotationHandle',
+  rotationHandleSize: 14,
+  rotationTooltipName: 'rotationTooltip',
+  shortcuts: false,
+  singleSelect: false,
+  snapConstraint: true,
+  tooltipZ: 1100,
+};
 export const isGroupId = id => id.startsWith(aeroelasticConfiguration.groupName);
 
 /**
@@ -286,4 +320,33 @@ export const layoutEngine = ({
       }
     },
   };
+};
+
+export const calcNextStateFromRedux = (store, shapes, selectedElement) => {
+  const selectedShapeObjects = [selectedElement]
+    .map(e => shapes.find(s => s.id === e))
+    .filter(s => s);
+
+  const prevSelectionState = (store && store.currentScene && store.currentScene.selectionState) || {
+    shapes: [],
+    uid: Math.round(1000000000 * Math.random()),
+    depthIndex: 0,
+    down: false,
+  };
+  return createLayoutStore({
+    primaryUpdate: null,
+    currentScene: {
+      shapes,
+      configuration: aeroelasticConfiguration,
+      selectedShapes: [selectedElement],
+      selectedLeafShapes: [selectedElement],
+      selectedPrimaryShapes: [selectedElement],
+      selectedShapeObjects,
+      selectionState: {
+        shapes: selectedShapeObjects,
+        uid: prevSelectionState.uid, // + 1,
+        ...prevSelectionState,
+      },
+    },
+  });
 };
--- x-pack/plugins/canvas/public/lib/aeroelastic_kibana.js
+++ x-pack/plugins/canvas/public/lib/aeroelastic_kibana.js
@@ -4,76 +4,16 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-import { createLayoutStore, matrix } from './aeroelastic';
+import { matrix } from './aeroelastic';
+import { calcNextStateFromRedux } from './aeroelastic/integration_utils';
 
 let store;
-
-export const aeroelasticConfiguration = {
-  getAdHocChildAnnotationName: 'adHocChildAnnotation',
-  adHocGroupName: 'adHocGroup',
-  alignmentGuideName: 'alignmentGuide',
-  atopZ: 1000,
-  depthSelect: true,
-  devColor: 'magenta',
-  groupName: 'group',
-  groupResize: true,
-  guideDistance: 3,
-  hoverAnnotationName: 'hoverAnnotation',
-  hoverLift: 100,
-  intraGroupManipulation: false,
-  intraGroupSnapOnly: false,
-  minimumElementSize: 2,
-  persistentGroupName: 'persistentGroup',
-  resizeAnnotationConnectorOffset: 0,
-  resizeAnnotationOffset: 0,
-  resizeAnnotationOffsetZ: 0.1, // causes resize markers to be slightly above the shape plane
-  resizeAnnotationSize: 10,
-  resizeConnectorName: 'resizeConnector',
-  resizeHandleName: 'resizeHandle',
-  rotateAnnotationOffset: 12,
-  rotateSnapInPixels: 10,
-  rotationEpsilon: 0.001,
-  rotationHandleName: 'rotationHandle',
-  rotationHandleSize: 14,
-  rotationTooltipName: 'rotationTooltip',
-  shortcuts: false,
-  singleSelect: false,
-  snapConstraint: true,
-  tooltipZ: 1100,
-};
 
 export const aeroelastic = {
   matrix,
 
   setStore(shapes, selectedElement) {
-    const selectedShapeObjects = [selectedElement]
-      .map(e => shapes.find(s => s.id === e))
-      .filter(s => s);
-
-    const prevSelectionState = (store &&
-      store.currentScene &&
-      store.currentScene.selectionState) || {
-      shapes: [],
-      uid: Math.round(1000000000 * Math.random()),
-      depthIndex: 0,
-      down: false,
-    };
-    store = createLayoutStore({
-      primaryUpdate: null,
-      currentScene: {
-        shapes,
-        configuration: aeroelasticConfiguration,
-        selectedShapes: [selectedElement],
-        selectedLeafShapes: [selectedElement],
-        selectedPrimaryShapes: [selectedElement],
-        selectedShapeObjects,
-        selectionState: {
-          shapes: selectedShapeObjects,
-          uid: prevSelectionState.uid, // + 1,
-          ...prevSelectionState,
-        },
-      },
-    });
+    store = calcNextStateFromRedux(store, shapes, selectedElement);
   },
 
   getStore() {
