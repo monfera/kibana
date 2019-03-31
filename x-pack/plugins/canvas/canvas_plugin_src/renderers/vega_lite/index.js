4924e5d9ee3ad16612376439ed33d8f5c9422181
monfera <monfera.robert@gmail.com>
2019-03-31 19:33:50 +0200

    chart shows up

--- x-pack/plugins/canvas/canvas_plugin_src/renderers/vega_lite/index.js
+++ x-pack/plugins/canvas/canvas_plugin_src/renderers/vega_lite/index.js
@@ -4,65 +4,45 @@
  * you may not use this file except in compliance with the Elastic License.
  */
 
-// This bit of hackiness is required because this isn't part of the main kibana bundle
-import 'jquery';
-import '../../lib/flot-charts';
-import * as vegaLite from 'vega-lite'; // eslint-disable-line import/no-extraneous-dependencies
+// import * as vega from 'vega-lib'; // eslint-disable-line import/no-extraneous-dependencies
+// import * as vegaLite from 'vega-lite'; // eslint-disable-line import/no-extraneous-dependencies
+import Vega from 'react-vega'; // eslint-disable-line import/no-extraneous-dependencies
+import React from 'react';
 
-console.log(vegaLite);
+import * as ReactDOM from 'react-dom';
 
-import { debounce, includes } from 'lodash';
-import { size } from './plugins/size';
-import { text } from './plugins/text';
+const spec = JSON.parse(
+  `{"$schema":"https://vega.github.io/schema/vega/v3.0.json","description":"A simple bar chart with embedded data.","autosize":"pad","padding":5,"style":"cell","data":[{"name":"source","values":[{"x":40,"y":"A"},{"x":20,"y":"B"},{"x":50,"y":"C"},{"x":5,"y":"D"}]},{"name":"selected","on":[{"trigger":"clicked","toggle":"clicked"}]}],"signals":[{"name":"width","update":"600"},{"name":"height","update":"400"},{"name":"clicked","value":null,"on":[{"events":"@marks:click","update":"{value: datum.y}","force":true}]}],"marks":[{"name":"marks","type":"rect","style":["bar"],"from":{"data":"source"},"encode":{"update":{"x":{"scale":"x","field":"x"},"x2":{"scale":"x","value":0},"y":{"scale":"y","field":"y"},"height":{"scale":"y","band":true},"fill":[{"test":"(!length(data('selected')) || indata('selected', 'value', datum.y))","scale":"color","field":"y"},{"value":"grey"}]}}}],"scales":[{"name":"x","type":"linear","domain":{"data":"source","field":"x"},"range":[0,{"signal":"width"}],"round":true,"nice":true,"zero":true},{"name":"y","type":"band","domain":{"data":"source","field":"y","sort":true},"range":[{"signal":"height"},0],"round":true,"paddingInner":0.1,"paddingOuter":0.05},{"name":"color","type":"ordinal","domain":{"data":"source","field":"y","sort":true},"range":"category"}],"axes":[{"scale":"x","labelOverlap":true,"orient":"bottom","tickCount":{"signal":"ceil(width/40)"},"title":"x in cursive font (see config)","zindex":1},{"scale":"x","domain":false,"grid":true,"labels":false,"maxExtent":0,"minExtent":0,"orient":"bottom","tickCount":{"signal":"ceil(width/40)"},"ticks":false,"zindex":0,"gridScale":"y"},{"scale":"y","labelOverlap":true,"orient":"left","title":"y in cursive font (see config)","zindex":1}],"config":{"axis":{"domainColor":"#888","tickColor":"#888","labelFont":"cursive","titleFont":"cursive"},"axisY":{"minExtent":30}}}`
+);
+
+const Chart = () => {
+  return (
+    <div className="Chart" style={{ width: 800, height: 400 }}>
+      <Vega spec={spec} width={800} />
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Chart />
+    </div>
+  );
+}
+
+/*
+const draw = domNode => {
+  const width = domNode.offsetWidth;
+  const height = domNode.offsetHeight;
+}
+*/
 
 const render = (domNode, config, handlers) => {
-  // TODO: OH NOES
-  if (!includes($.plot.plugins, size)) {
-    $.plot.plugins.push(size);
-  }
-  if (!includes($.plot.plugins, text)) {
-    $.plot.plugins.push(text);
-  }
+  handlers.onDestroy(() => {});
+  handlers.onResize(() => {});
 
-  let plot;
-  function draw() {
-    if (domNode.clientHeight < 1 || domNode.clientWidth < 1) {
-      return;
-    }
-
-    if (config.font) {
-      const legendFormatter = label => {
-        const labelSpan = document.createElement('span');
-        Object.assign(labelSpan.style, config.font.spec);
-        labelSpan.textContent = label;
-        return labelSpan.outerHTML;
-      };
-      config.options.legend.labelFormatter = legendFormatter;
-    }
-
-    try {
-      if (!plot) {
-        plot = $.plot($(domNode), config.data, config.options);
-      } else {
-        plot.resize();
-        plot.setupGrid();
-        plot.draw();
-      }
-    } catch (e) {
-      // Nope
-    }
-  }
-
-  function destroy() {
-    if (plot) {
-      plot.shutdown();
-    }
-  }
-
-  handlers.onDestroy(destroy);
-  handlers.onResize(debounce(draw, 40, { maxWait: 40 })); // 1000 / 40 = 25fps
-
-  draw();
+  ReactDOM.render(<App />, domNode);
 
   return handlers.done();
 };
